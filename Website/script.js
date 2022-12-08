3c8f3df6d20bc94875df55ce9591232ef8e6d90d
NassimAlaimi <nassim.alaimi@epitech.eu>
2022-12-08 10:12:56 +0100

    [FIX]: fix zoom

--- Website/script.js
+++ Website/script.js
@@ -1,16 +1,24 @@
-//Création de la carte
+//Option de la carte
 let mapOptions = {
     center:[47.237054, -1.565895],
-    zoom:12
+    zoom:12,
+    zoomControl: false
 }
+
+// Option des layers
+let layerOptions = {
+    attribution: 'Scampoule !',
+    maxNativeZoom: 18,
+    maxZoom: 25
+}
+
+//Création de la carte
 let map = new L.map('map' , mapOptions);
 
 //Ajout des couches
-let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-var dark_layer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-	attribution: '© <a href="https://stadiamaps.com/">Stadia Maps</a>, © <a href="https://openmaptiles.org/">OpenMapTiles</a> © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-});
-map.addLayer(dark_layer);
+let layer = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', layerOptions);
+// let dark_layer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', layerOptions);
+map.addLayer(layer);
 
 //Gestion du clic sur la carte
 var popup = L.popup();
@@ -25,25 +33,37 @@ function onMapClick(e) {
 map.on('click', onMapClick);
 
 //Gestion des positions des markers
+
 let layerGroupArray = [];
 let markerArray = [];
 let filterArray = [];
 
 function parseData(json, map, layerGroupArray, markerArray) {
-    var markers = L.markerClusterGroup();
-    var customOptions =
-    {
+    //Création des clusters et de leurs icones
+    var markers = L.markerClusterGroup({
+        //Modifie l'icone des clusters
+        // iconCreateFunction: function(cluster) {
+        //     return L.divIcon({ html: '<b>' + cluster.getChildCount() + '</b>' });
+        // }
+        singleMarkerMode: false, //Affiche uniquement des clusters
+        spiderfyOnMaxZoom: false, //Désactive le spiderfy --> je sais plus c'est quoi mais c'est cool, vas de paire avec disableClusteringAtZoom donc à laisser
+        disableClusteringAtZoom: 20 //Désactive le clustering à partir du zoom
+    });
+
+    //Options css des popups
+    var customOptions = {
         'maxWidth': '500',
         'className' : 'custom'
     }
 
+    //Iterate through the JSON array.
     for (let i = 0; i < json.length; i++) {
         let lat = json[i]['fields']['geo_point_2d'][0];
         let lng = json[i]['fields']['geo_point_2d'][1];
         let ville = json[i]['fields']['lib_com'];
         map = createMarker(map, lat, lng, ville, layerGroupArray, markerArray);
 
-
+        // Information sur les Lampadaires
         const compare = String(json[i]['fields']['type_lampe']).localeCompare('SHP');
 
         let type = String("<h1> Éclairage n° " + json[i]['fields']['numero'] + "</h1>");
@@ -54,10 +74,10 @@ function parseData(json, map, layerGroupArray, markerArray) {
         type += String("<h2><u>Conso:</u><br/> 34 kW/h</h2>");
         type += String("<h2><u>Émission (CO2):</u><br/> 14 gr de CO2</h2>");
 
-
         let marker = new L.Marker([lat, lng]).bindPopup(type, customOptions);
         markers.addLayer(marker);
     }
+    //Ajout des cluster dans la carte
     map.addLayer(markers);
     filterArray = getAndApplyFilter(json);
     return map;
@@ -120,3 +140,11 @@ function readData(map) {
 }
 
 map = readData(map);
+
+//Gestion du print du zoom
+map.on('zoomend', showZoomLevel);
+showZoomLevel();
+
+function showZoomLevel() {
+    document.getElementById('zoom').innerHTML = map.getZoom();
+}
