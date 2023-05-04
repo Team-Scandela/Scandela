da4d5d88a1715e457dd2f7d39260fe229ea095dc
TitouanDsch <titouandeschanels@users.noreply.github.com>
2023-05-04 15:41:54 +0200

    Add login module in a new blank page

---
+++ scandelapp/src/components/LoginModule/elements.js
@@ -0,0 +1,170 @@
+import styled from 'styled-components';
+
+export const LoginContainer = styled.div`
+    background-color: #393E46;
+    border-radius: 10px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+    position: relative;
+    overflow: hidden;
+    width: 678%;
+    max-width: 50%;
+    min-height: 400px;
+    top : 50%;
+    left : 50%;
+    transform: translate(-50%, 50%);
+`;
+
+export const SignUpContainer = styled.div`
+    background-color: #fff;
+    position: absolute;
+    top: 0;
+    height: 100%;
+    width: 50%;
+    left: 0;
+    transition : all 0.5s ease-in-out;
+    opacity: 0;
+    z-index:1;
+    ${props => props.signinIn !== true ? ` transform: translateX(100%); opacity: 1; z-index: 5;` : null}
+`;
+
+export const SignInContainer = styled.div`
+    background-color: #fff;
+    position: absolute;
+    top: 0;
+    height: 100%;
+    width: 50%;
+    left: -50%;
+    transition : all 0.5s ease-in-out;
+    opacity: 0;
+    z-index:1;
+    ${props => props.signinIn !== true ? ` transform: translateX(100%); opacity: 1; z-index: 5;` : null}
+`;
+
+export const Form = styled.form`
+    background-color: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    padding: 0 50px;
+    text-align: center;
+`;
+
+export const Title = styled.h1`
+    font-weight : bold;
+    margin : 0;
+`;
+
+export const Input = styled.input`
+    background-color: #eee;
+    border: none;
+    padding: 12px 15px;
+    margin: 8px 0;
+    width: 100%;
+`;
+
+export const Button = styled.button`
+    border-radius: 20px;
+    border: 1px solid #01bf71;
+    background-color: #01bf71;
+    color: #ffffff;
+    font-size: 12px;
+    font-weight: bold;
+    padding: 12px 45px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    transition: transform 80ms ease-in;
+    margin-top: 20px;
+
+    &:hover {
+        cursor: pointer;
+        transform: scale(1.05);
+        box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+    }
+
+    &:active{
+        transform: scale(0.95);
+    }
+
+`;
+
+export const GhostButton = styled(Button)`
+    border-color: #ffffff;
+    margin-top: 10px;
+
+    &:hover {
+        cursor: pointer;
+        transform: scale(1.05);
+    }
+
+    &:active{
+        transform: scale(0.95);
+    }
+`;
+
+export const Anchor = styled.a`
+    color: #333;
+    font-size: 14px;
+    text-decoration: underline;
+    margin: 15px 0;
+
+`;
+
+export const OverlayContainer = styled.div`
+    position: absolute;
+    top: 0;
+    left: 50%;
+    width: 50%;
+    height: 100%;
+    overflow: hidden;
+    transition: transform 0.6s ease-in-out;
+    z-index: 999;
+    ${props =>props.signinIn !== true ? `transform: translateX(-100%);` : null}
+`;
+
+export const Overlay = styled.div`
+    background: #01bf71;
+    color: #ffffff;
+    position: relative;
+    left: -100%;
+    height: 100%;
+    width: 200%;
+    transform: translateX(0);
+    transition: transform 0.6s ease-in-out;
+    ${props => (props.signinIn !== true ? `transform: translateX(50%);` : null)}
+`;
+
+
+export const OverlayPanel = styled.div`
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    height: 100%;
+    width: 50%;
+    transform: translateX(0);
+    transition: transform 0.6s ease-in-out;
+`;
+
+// Move the text for the animation
+export const LeftOverlayPanel = styled(OverlayPanel)`
+    transform: translateX(-20%);
+    ${props => props.signinIn !== true ? `transform: translateX(0);` : null}
+`;
+
+export const RightOverlayPanel = styled(OverlayPanel)`
+    right: 0;
+    transform: translateX(0);
+    ${props => props.signinIn !== true ? `transform: translateX(20%);` : null}
+`;
+
+export const Paragraph = styled.p`
+    font-size: 14px;
+    font-weight: 100;
+    line-height: 20px;
+    letter-spacing: 0.5px;
+    margin: 20px 0 30px
+`;
