7be8b1928c55b95f2078ecae5b44dcbc2bf0b3cd
Enzo Bonato <enzo.bonato@epitech.eu>
2023-05-19 15:15:18 +0200

    [EB]: [EDIT]: re write index of EditButtonInPdf in types

--- scandelapp/src/components/ButtonEditInPdf/index.tsx
+++ scandelapp/src/components/ButtonEditInPdf/index.tsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import * as React from 'react'
 import { ButtonEditContainer, ExportIcon } from './elements'
 
 /** Button EditInPDf of the Decison Help Menu
  * This Button allow the user to summarize all the infos that Scandela 
  * will give him about how to decrease his consumption in electricity
  * @param {boolean} isDark - If the mode is dark or not
+ * 
 **/
-const ButtonEditInPdf = ( { isDark } ) => {
-    return (
-        <ButtonEditContainer isDark={isDark}>
-            <ExportIcon isDark={isDark} />
-        </ButtonEditContainer>
 
+interface ButtonEditInPdfProps {
+    isDark: boolean;
+}
+
+const ButtonEditInPdf: React.FC<ButtonEditInPdfProps> = ({ isDark }) => {
+    return (
+        <div>
+            <ButtonEditContainer isDark={isDark}>
+                <ExportIcon isDark={isDark} />
+            </ButtonEditContainer>
+        </div>
     )
 }
 
