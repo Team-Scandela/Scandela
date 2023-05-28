5a921f0614f0b6947e7f5a2dbe673df6fad4f64e
Laurent Enzo <enzo.laurent@epitech.eu>
2023-05-28 12:12:50 +0200

    [EDIT] Return 2 strings as a list

--- server/src/main/java/com/scandela/server/controller/ServerController.java
+++ server/src/main/java/com/scandela/server/controller/ServerController.java
@@ -1,5 +1,8 @@
 package com.scandela.server.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ServerController extends AbstractController {
 
 	@GetMapping
-	public String getServerInfo() {
-		return "";
+	public List<String> getServerInfo() {
+		List<String> a = new ArrayList<>();
+		
+		a.add("Test1");
+		a.add("Test2");
+		return a;
 	}
 
 }
