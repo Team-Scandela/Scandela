2576ab2dcb35953ce7df0a7857a32465c61d18a3
Ismaël Brossaud <ismael.brossaud@epitech.eu>
2023-05-29 16:25:02 +0200

    [FIX] better deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Build images docker
+echo "Build docker images"
+sudo docker-compose -f docker-compose.yml build
+
 # Transférer les fichiers sur le Raspberry Pi
-echo "Transfert des fichiers vers le Raspberry Pi..."
+echo "Transfer files to Raspberry Pi..."
 scp -r ./scandelapp pi@<adresse_ip_raspberry>:/home/pi/scandelapp
 scp -r ./server pi@<adresse_ip_raspberry>:/home/pi/server
 scp -r ./jenkins pi@<adresse_ip_raspberry>:/home/pi/jenkins
 
 # Connexion au Raspberry Pi
-echo "Connexion au Raspberry Pi..."
+echo "Connexion to Raspberry Pi..."
 ssh pi@<adresse_ip_raspberry> << EOF
   cd /home/pi
 
@@ -16,4 +20,4 @@ ssh pi@<adresse_ip_raspberry> << EOF
   docker-compose up -d
 EOF
 
-echo "Déploiement terminé !"
+echo "Deploy done !"
