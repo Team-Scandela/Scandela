8b0e4b38cee4e194185473ba75956bb3609e1334
Enzo Bonato <enzo.bonato@epitech.eu>
2023-06-05 12:22:29 +0200

    [EB] [ADD]: Download a pdf on click

--- scandelapp/src/components/EditInPdfPannel/index.tsx
+++ scandelapp/src/components/EditInPdfPannel/index.tsx
@@ -1,28 +1,42 @@
-import * as React from 'react'
-import { PannelContainer, ExportButton, PannelText, ExportText, ExportIcon} from './elements'
-
-/** Button EditInPDf of the Decison Help Menu
- * This Button allow the user to summarize all the infos that Scandela 
- * will give him about how to decrease his consumption in electricity
- * @param {boolean} isDark - If the mode is dark or not
-**/
+import * as React from 'react';
+import { PannelContainer, ExportButton, PannelText, ExportText, ExportIcon } from './elements';
+import { PDFDocument } from 'pdf-lib';
 
 interface EditInPdfPannellProps {
-    isDark: boolean;
+  isDark: boolean;
 }
 
 const EditInPdfPannel: React.FC<EditInPdfPannellProps> = ({ isDark }) => {
-    return (
-        <div>
-            <PannelContainer isDark={isDark}> 
-              <PannelText isDark={isDark}> Exporter les actions Sélectionnées </PannelText>
-              <ExportButton isDark={isDark}> 
-                <ExportText isDark={isDark}> Selectionner le dossier de téléchargement </ExportText>
-                <ExportIcon isDark={isDark}/>
-              </ExportButton>
-            </PannelContainer>
-        </div>
-    )
-}
+  const handleButtonClick = async () => {
+    const pdfDoc = await PDFDocument.create();
+    const pdfBytes = await pdfDoc.save();
 
-export default EditInPdfPannel
+    const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'optimisation_scandela.pdf';
+
+    link.style.display = 'none';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    URL.revokeObjectURL(url);
+  };
+
+  return (
+    <div>
+      <PannelContainer isDark={isDark}>
+        <PannelText isDark={isDark}>Exporter les actions Sélectionnées</PannelText>
+        <ExportButton isDark={isDark}>
+          <ExportText isDark={isDark}>Télécharger le fichier PDF</ExportText>
+          <ExportIcon isDark={isDark} onClick={handleButtonClick} />
+        </ExportButton>
+      </PannelContainer>
+    </div>
+  );
+};
+
+export default EditInPdfPannel;
