8acb1090f2c7ef0c0cc0c5d64cd879910c16065f
Laurent Enzo <enzo.laurent@epitech.eu>
2023-06-08 11:50:51 +0200

    [ADD] TODO when the db will be available

--- server/src/main/java/com/scandela/server/ServerApplication.java
+++ server/src/main/java/com/scandela/server/ServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })//TODO enlever le exclude quand la db sera setup et que la co a la db sera setup
 public class ServerApplication {
 
 	public static void main(String[] args) {
