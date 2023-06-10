faaec7ec51d7ae22e6dac60c383def551a8004fb
Enzo Bonato <enzo.bonato@epitech.eu>
2023-06-10 12:36:35 +0200

    [EB] [ADD]: Write basic texte on pdf

--- scandelapp/src/components/EditInPdfPannel/index.tsx
+++ scandelapp/src/components/EditInPdfPannel/index.tsx
@@ -1,42 +1,21 @@
 import * as React from 'react';
 import { PannelContainer, ExportButton, PannelText, ExportText, ExportIcon } from './elements';
-import { PDFDocument } from 'pdf-lib';
+import { saveAs } from 'file-saver';
+import { generatePDFDocument } from './pdfGenerator';
 
 interface EditInPdfPannellProps {
   isDark: boolean;
   isButtonEditInPdfClicked: boolean;
 }
 
-const EditInPdfPannel: React.FC<EditInPdfPannellProps> = ({ isDark, isButtonEditInPdfClicked}) => {
-  const fileInputRef = React.useRef<HTMLInputElement>(null);
-
+const EditInPdfPannel: React.FC<EditInPdfPannellProps> = ({ isDark, isButtonEditInPdfClicked }) => {
   const handleButtonClick = async () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
-  };
-
-  const handleFileSelection = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      const pdfDoc = await PDFDocument.create();
-      // Ajoutez ici le contenu au document PDF
-
-      const pdfBytes = await pdfDoc.save();
-
+    try {
+      const pdfBytes = await generatePDFDocument();
       const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-      const url = URL.createObjectURL(blob);
-
-      const link = document.createElement('a');
-      link.href = url;
-      link.download = 'optimisation_scandela.pdf';
-
-      link.style.display = 'none';
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-
-      URL.revokeObjectURL(url);
+      saveAs(blob, 'optimisation_scandela.pdf');
+    } catch (error) {
+      console.error('Error generating PDF:', error);
     }
   };
 
@@ -45,16 +24,10 @@ const EditInPdfPannel: React.FC<EditInPdfPannellProps> = ({ isDark, isButtonEdit
       <PannelContainer isDark={isDark} isButtonEditInPdfClicked={isButtonEditInPdfClicked}>
         <PannelText isDark={isDark}>Exporter les actions Sélectionnées</PannelText>
         <ExportButton isDark={isDark}>
-          <ExportText isDark={isDark}>Ouvrir le gestionnaire de fichiers</ExportText>
+          <ExportText isDark={isDark}>Télécharger le fichier PDF</ExportText>
           <ExportIcon isDark={isDark} onClick={handleButtonClick} />
         </ExportButton>
       </PannelContainer>
-      <input
-        ref={fileInputRef}
-        type="file"
-        style={{ display: 'none' }}
-        onChange={handleFileSelection}
-      />
     </div>
   );
 };
