45d26379911ac14202312cf108de805586328145
Ismael Brossaud <ismael.brossaud@epitech.eu>
2023-06-20 15:37:09 +0200

    fix dockerfile port and volume

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,57 @@
 #!/bin/bash
 
-# Build images docker
-echo "Build docker images"
-sudo docker-compose -f docker-compose.yml build
+# Vérifier le nombre d'arguments
+if [ $# -ne 1 ]; then
+    echo "Utilisation : $0 [dev|prod]"
+    exit 1
+fi
 
-# Transférer les fichiers sur le Raspberry Pi
-echo "Transfer files to Raspberry Pi..."
-scp -r ./scandelapp pi@<adresse_ip_raspberry>:/home/pi/scandelapp
-scp -r ./server pi@<adresse_ip_raspberry>:/home/pi/server
-scp -r ./jenkins pi@<adresse_ip_raspberry>:/home/pi/jenkins
+# Vérifier si Docker est installé
+if ! command -v docker &> /dev/null; then
+    echo "Docker n'est pas installé. Veuillez l'installer avant de continuer."
+    exit 1
+fi
 
-# Connexion au Raspberry Pi
-echo "Connexion to Raspberry Pi..."
-ssh pi@<adresse_ip_raspberry> << EOF
-  cd /home/pi
+# Vérifier si git est installé
+if ! command -v git &> /dev/null; then
+    echo "Git n'est pas installé. Veuillez l'installer avant de continuer."
+    exit 1
+fi
 
-  # Build et déploiement des conteneurs
-  echo "Construction et déploiement des conteneurs Docker..."
-  docker-compose up -d
-EOF
+# Récupérer l'environnement spécifié (dev ou prod)
+ENVIRONMENT=$1
 
-echo "Deploy done !"
+# Vérifier l'environnement et effectuer les actions appropriées
+if [ "$ENVIRONMENT" = "dev" ]; then
+    REPO_URL="https://github.com/Team-Scandela/Scandela.git"
+    CLONE_DIR="dev_env"
+    COMPOSE_DIR="dev/Scandela"
+elif [ "$ENVIRONMENT" = "prod" ]; then
+    REPO_URL="https://github.com/Team-Scandela/Scandela.git"
+    CLONE_DIR="app"
+    COMPOSE_DIR="app/Scandela"
+else
+    echo "Environnement non valide. Utilisation : $0 [dev|prod]"
+    exit 1
+fi
+
+# Vérifier si le dossier Scandela existe
+if [ -d "$COMPOSE_DIR" ]; then
+    # Se déplacer dans le répertoire Scandela
+    cd "$COMPOSE_DIR"
+    
+    # Mettre à jour le dépôt existant
+    git pull
+else
+    # Cloner le dépôt GitHub dans le dossier spécifié
+    git clone "$REPO_URL" "$CLONE_DIR"
+    
+    # Se déplacer dans le répertoire cloné
+    cd "$CLONE_DIR"
+fi
+
+# Lancer le docker-compose dans le dossier Scandela
+if [ "$ENVIRONMENT" = "dev" ]; then
+    docker compose -f docker-compose.dev.yml up -d --remove-orphans
+else
+    docker compose up -d --remove-orphans
