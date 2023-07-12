559bb6e1b5571cbcdcc6bfd5f006a67bfa9471e4
TitouanDsch <titouandeschanels@users.noreply.github.com>
2023-07-12 12:03:26 +0200

    Insert call work

--- Database/src/index.ts
+++ Database/src/index.ts
@@ -16,42 +16,18 @@ app.use( function(req : Request, res : Response, next: NextFunction) {
 
 app.get('/lamp', (request : Request, response : Response) => {
     lamp_model.getLamp(request, response)
-    .then((result : any) => {
-        response.status(200).json(result.rows);
-    })
-    .catch( (error : any) => {
-        throw error;
-    });
 });
 
 app.post('/lamp', (request : Request, response : Response) => {
     lamp_model.createLamp(request, response)
-    .then((result : any) => {
-        response.status(201).send(`Lamp added with ID: ${result.rows[0].uuid}`);
-    })
-    .catch( (error : any) => {
-        throw error;
-    });
 });
 
 app.delete('/lamp/:uuid', (request : Request, response : Response) => {
     lamp_model.deleteLamp(request, response)
-    .then((result : any) => {
-        response.status(200).send(`Lamp deleted with ID: ${request.params.uuid}`);
-    })
-    .catch( (error : any) => {
-        throw error;
-    });
 });
 
 app.patch('/lamp/:uuid', (request : Request, response : Response) => {
     lamp_model.updateLamp(request, response)
-    .then((result : any) => {
-        response.status(200).send(`Lamp modified with ID: ${request.params.uuid}`);
-    })
-    .catch( (error : any) => {
-        throw error;
-    });
 });
 
 app.listen(port, () => {
--- Database/src/lamp_model.ts
+++ Database/src/lamp_model.ts
@@ -1,62 +1,54 @@
 const Pool = require('pg').Pool
 require('dotenv').config();
-import { uuid } from 'uuidv4';
+import { v4 as uuidv4 } from 'uuid';
 import { Request, Response } from 'express';
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'scandb',
-    password: 'scandatabase85',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 })
 
 const getLamp = (request : Request, response : Response) => {
-    return new Promise(function(resolve, reject) {
-        pool.query('SELECT * FROM \"Lamp\"', (error : any, results : any) => {
-            if (error) {
-                reject(error);
-            }
-            resolve(results);
-        });
+    pool.query('SELECT * FROM \"Lamp\"', (error : any, results : any) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
     });
 }
 
 const createLamp = (request : Request, response : Response) => {
-    return new Promise(function(resolve, reject) {
-        const {lat, lng} = request.body;
-        pool.query('INSERT INTO \"Lamp\" (UUID, lat, lng) VALUES ($1, $2, $3) RETURNING *', [uuid(), lat, lng], (error : any, results : any) => {
-            if (error) {
-                reject(error);
-            }
-            console.log("results \n\n\n" + results)
-            resolve(`Lamp added with ID: ${results}`)
-        });
+    const {lat, lng} = request.body;
+    pool.query('INSERT INTO \"Lamp\" (\"UUID\", lat, lng) VALUES ($1, $2, $3) RETURNING *', [uuidv4(), lat, lng], (error : any, results : any) => {
+        if (error) {
+            throw error;
+        }
+        response.status(201).send(`Lamp added with ID: ${results.rows[0].UUID}`);
     });
 }
 
 const deleteLamp = (request : Request, response : Response) => {
-    return new Promise(function(resolve, reject) {
-        const uuid = request.params.uuid;
-        pool.query('DELETE FROM \"Lamp\" WHERE UUID = $1', [uuid], (error : any, results : any) => {
-            if (error) {
-                reject(error)
-            }
-            resolve(`Lamp deleted with ID: ${uuid}`)
-        });
+    const uuid = request.params.uuid;
+    pool.query('DELETE FROM \"Lamp\" WHERE UUID = $1', [uuid], (error : any, results : any) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).send(`Lamp deleted with ID: ${uuid}`);
     });
+
 }
 
 const updateLamp = (request : Request, response : Response) => {
-    return new Promise(function(resolve, reject) {
-        const uuid = request.params.uuid;
-        const {lat, lng} = request.body;
-        pool.query('UPDATE \"Lamp\" SET lat = $1, lng = $2 WHERE UUID = $3', [lat, lng, uuid], (error : any, results : any) =>{
-            if (error) {
-                reject(error);
-            }
-            resolve(`Lamp modified with ID: ${uuid}`)
-        });
+    const uuid = request.params.uuid;
+    const {lat, lng} = request.body;
+    pool.query('UPDATE \"Lamp\" SET lat = $1, lng = $2 WHERE UUID = $3', [lat, lng, uuid], (error : any, results : any) =>{
+        if (error) {
+            throw error;
+        }
+        response.status(200).send(`Lamp modified with ID: ${uuid}`);
     });
 }
 
