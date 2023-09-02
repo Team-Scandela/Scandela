a4160b762593a6e5f4cf035fd631bdb0e76ea48b
Enzo Bonato <enzo.bonato@epitech.eu>
2023-09-02 20:39:16 +0200

    [EB] [ADD]: First succes to display some clusters

--- scandelapp/src/components/Map/index.tsx
+++ scandelapp/src/components/Map/index.tsx
@@ -1,17 +1,14 @@
 import * as mapboxgl from 'mapbox-gl';
-import * as React from 'react'
+import Supercluster from 'supercluster';
+import * as React from 'react';
 
 let nantesData = require('../../assets/nantesData.json');
 
 Object.getOwnPropertyDescriptor(mapboxgl, "accessToken").set("pk.eyJ1IjoidGl0b3VhbnRkIiwiYSI6ImNsaDYyeHUybDAyNTkzcHV5NHlzY3drbHIifQ._eEX5CRcWxVrl9C8z4u3fQ");
 
-/** Map of the city
- * @param {boolean} isDark - If the map is in dark mode or not
-*/
-
 interface MapProps {
-    id : string,
-    filter : string,
+    id: string,
+    filter: string,
     isDark: boolean,
     lat: number,
     lng: number,
@@ -19,11 +16,10 @@ interface MapProps {
 }
 
 const Map: React.FC<MapProps> = ({ id, filter, isDark, lat, lng, zoom }) => {
-
     const mapContainer = React.useRef<HTMLDivElement | null>(null);
     const map = React.useRef<mapboxgl.Map | null>(null);
+    const cluster = React.useRef<Supercluster | null>(null);
 
-    //* Transform the data (format json) to geojson */
     const geojsonData = React.useMemo(() => {
         let geoJSON = {
             "type": "FeatureCollection",
@@ -44,11 +40,9 @@ const Map: React.FC<MapProps> = ({ id, filter, isDark, lat, lng, zoom }) => {
             geoJSON.features.push(feature);
         });
         return geoJSON;
-    },[]);
+    }, []);
 
-
-    /** Setup the map and change the style of the map wether is light or dark mode */
-    React.useEffect(() => {
+    const initializeMap = () => {
         if (map.current) {
             map.current.setStyle(
                 isDark ? "mapbox://styles/titouantd/cljwv2coy025k01pk785839a1" : "mapbox://styles/titouantd/cljwui6ss00ij01pj1oin6oa5"
@@ -56,31 +50,35 @@ const Map: React.FC<MapProps> = ({ id, filter, isDark, lat, lng, zoom }) => {
             map.current.flyTo({
                 center: [lng, lat],
                 zoom: zoom,
-                speed: 1.2, // Speed of the animation
-                curve: 1.42, // How the zooming is animated (curve factor)
+                speed: 1.2,
+                curve: 1.42,
             });
 
-            //* Add the markers through colored pins */
             map.current.on('load', () => {
                 map.current.addSource('points', {
                     type: 'geojson',
                     data: geojsonData as GeoJSON.FeatureCollection,
                 });
-                    map.current.addLayer({
-                        'id': 'coloredPin',
-                        'type': 'circle',
-                        'source': 'points',
-                        'layout': {},
-                        'paint': {
-                            'circle-radius': 6,
-                            'circle-color': '#FAC710',
-                            'circle-stroke-color': "#F9F9F9",
-                            'circle-stroke-width': 2
-                        }
-                    });
+                map.current.addLayer({
+                    'id': 'coloredPin',
+                    'type': 'circle',
+                    'source': 'points',
+                    'layout': {},
+                    'paint': {
+                        'circle-radius': 6,
+                        'circle-color': '#FAC710',
+                        'circle-stroke-color': "#F9F9F9",
+                        'circle-stroke-width': 2
+                    }
+                });
             });
+        } else if (!cluster.current) {
+            cluster.current = new Supercluster({
+                radius: 40,
+                maxZoom: 15,
+            });
+            cluster.current.load(geojsonData.features);
 
-        } else {
             map.current = new mapboxgl.Map({
                 container: mapContainer.current,
                 style: isDark ? "mapbox://styles/titouantd/cljwv2coy025k01pk785839a1" : "mapbox://styles/titouantd/cljwui6ss00ij01pj1oin6oa5",
@@ -88,17 +86,61 @@ const Map: React.FC<MapProps> = ({ id, filter, isDark, lat, lng, zoom }) => {
                 zoom: zoom,
             });
 
+            map.current.on('load', () => {
+                map.current.addSource('points', {
+                    type: 'geojson',
+                    data: geojsonData as GeoJSON.FeatureCollection,
+                    cluster: true,
+                    clusterRadius: 40,
+                    clusterMaxZoom: 15,
+                });
+
+                map.current.addLayer({
+                    'id': 'clusters',
+                    'type': 'circle',
+                    'source': 'points',
+                    'filter': ['has', 'point_count'],
+                    'paint': {
+                        'circle-radius': [
+                            'step',
+                            ['get', 'point_count'],
+                            20,
+                            100,
+                            30,
+                            750,
+                            40,
+                        ],
+                        'circle-color': '#FAC710',
+                    },
+                });
+
+                map.current.addLayer({
+                    'id': 'cluster-markers',
+                    'type': 'circle',
+                    'source': 'points',
+                    'filter': ['!', ['has', 'point_count']],
+                    'paint': {
+                        'circle-radius': 6,
+                        'circle-color': '#FAC710',
+                        'circle-stroke-color': "#F9F9F9",
+                        'circle-stroke-width': 2,
+                    },
+                });
+            });
         }
+    };
+
+    React.useEffect(() => {
+        initializeMap();
     }, [isDark, lng, lat, zoom]);
 
-    /** Set the map to take the entire screen */
     const styleMap = {
         height: "100vh",
         width: "100vw",
-    }
+    };
 
     return (
-        <div id={id} style={{overflow: "hidden"}}>
+        <div id={id} style={{ overflow: "hidden" }}>
             <div style={styleMap} ref={mapContainer} className="map-container" />
             <style>
                 {`.mapboxgl-ctrl-logo {
@@ -109,7 +151,7 @@ const Map: React.FC<MapProps> = ({ id, filter, isDark, lat, lng, zoom }) => {
                 }`}
             </style>
         </div>
-    )
-}
+    );
+};
 
-export default Map
+export default Map;
