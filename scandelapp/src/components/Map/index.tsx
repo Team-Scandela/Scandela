ae9fb43a53832cca2c781c2608e8232b6ce15278
Enzo Bonato <enzo.bonato@epitech.eu>
2023-09-03 15:39:52 +0200

    [EB] [ADD]: Fix id problem

--- scandelapp/src/components/Map/index.tsx
+++ scandelapp/src/components/Map/index.tsx
@@ -43,36 +43,7 @@ const Map: React.FC<MapProps> = ({ id, filter, isDark, lat, lng, zoom }) => {
     }, []);
 
     const initializeMap = () => {
-        if (map.current) {
-            map.current.setStyle(
-                isDark ? "mapbox://styles/titouantd/cljwv2coy025k01pk785839a1" : "mapbox://styles/titouantd/cljwui6ss00ij01pj1oin6oa5"
-            );
-            map.current.flyTo({
-                center: [lng, lat],
-                zoom: zoom,
-                speed: 5,
-                curve: 10,
-            });
-
-            map.current.on('load', () => {
-                map.current.addSource('points', {
-                    type: 'geojson',
-                    data: geojsonData as GeoJSON.FeatureCollection,
-                });
-                map.current.addLayer({
-                    'id': 'coloredPin',
-                    'type': 'circle',
-                    'source': 'points',
-                    'layout': {},
-                    'paint': {
-                        'circle-radius': 6,
-                        'circle-color': '#FAC710',
-                        'circle-stroke-color': "#F9F9F9",
-                        'circle-stroke-width': 2
-                    }
-                });
-            });
-        } else if (!cluster.current) {
+        if (!map.current) {
             cluster.current = new Supercluster({
                 radius: 100,
                 maxZoom: 17,
@@ -95,85 +66,84 @@ const Map: React.FC<MapProps> = ({ id, filter, isDark, lat, lng, zoom }) => {
                         clusterRadius: 100,
                         clusterMaxZoom: 17,
                     });
+
+                    // Define les couleurs en format RGBA avec une opacité de 0.6
+                    const greenRGBA = 'rgba(0, 128, 0, 0.6)';
+                    const yellowRGBA = 'rgba(255, 255, 0, 0.6)';
+                    const orangeRGBA = 'rgba(255, 165, 0, 0.6)';
+
+                    // Define les couleurs de la bordure en format RGBA avec une opacité de 0.3
+                    const greenBorderRGBA = 'rgba(0, 128, 0, 0.3)';
+                    const yellowBorderRGBA = 'rgba(255, 255, 0, 0.3)';
+                    const orangeBorderRGBA = 'rgba(255, 165, 0, 0.3)';
+
+                    map.current.addLayer({
+                        'id': 'clusters',
+                        'type': 'circle',
+                        'source': 'points',
+                        'filter': ['has', 'point_count'],
+                        'paint': {
+                            'circle-radius': 19,
+                            'circle-color': [
+                                'step',
+                                ['get', 'point_count'],
+                                greenRGBA, // Couleur verte
+                                19,
+                                yellowRGBA, // Couleur jaune
+                                100,
+                                orangeRGBA, // Couleur orange
+                            ],
+                        },
+                    });
+
+                    map.current.addLayer({
+                        'id': 'cluster-border',
+                        'type': 'circle',
+                        'source': 'points',
+                        'filter': ['has', 'point_count'],
+                        'paint': {
+                            'circle-radius': 24,
+                            'circle-color': [
+                                'step',
+                                ['get', 'point_count'],
+                                greenBorderRGBA, // Couleur de bordure verte
+                                19,
+                                yellowBorderRGBA, // Couleur de bordure jaune
+                                100,
+                                orangeBorderRGBA, // Couleur de bordure orange
+                            ],
+                        },
+                    });
+
+                    map.current.addLayer({
+                        'id': 'cluster-markers',
+                        'type': 'circle',
+                        'source': 'points',
+                        'filter': ['!', ['has', 'point_count']],
+                        'paint': {
+                            'circle-radius': 6,
+                            'circle-color': '#FAC710',
+                            'circle-stroke-color': "#F9F9F9",
+                            'circle-stroke-width': 2,
+                        },
+                    });
+
+                    map.current.addLayer({
+                        'id': 'cluster-text',
+                        'type': 'symbol',
+                        'source': 'points',
+                        'filter': ['has', 'point_count'],
+                        'layout': {
+                            'text-field': '{point_count}',
+                            'text-font': ['Open Sans Regular'],
+                            'text-size': 13,
+                        },
+                        'paint': {
+                            'text-color': '#000000',
+                        },
+                    });
                 }
-
-                 // Define les couleurs en format RGBA avec une opacité de 0.6
-                const greenRGBA = 'rgba(0, 128, 0, 0.6)';
-                const yellowRGBA = 'rgba(255, 255, 0, 0.6)';
-                const orangeRGBA = 'rgba(255, 165, 0, 0.6)';
-
-                 // Define les couleurs de la bordure en format RGBA avec une opacité de 0.3
-                const greenBorderRGBA = 'rgba(0, 128, 0, 0.3)';
-                const yellowBorderRGBA = 'rgba(255, 255, 0, 0.3)';
-                const orangeBorderRGBA = 'rgba(255, 165, 0, 0.3)';
-                       
-                map.current.addLayer({
-                    'id': 'clusters',
-                    'type': 'circle',
-                    'source': 'points',
-                    'filter': ['has', 'point_count'],
-                    'paint': {
-                      'circle-radius': 19,
-                      'circle-color': [
-                        'step',
-                        ['get', 'point_count'],
-                        greenRGBA, // Couleur verte
-                        19,
-                        yellowRGBA, // Couleur jaune
-                        100,
-                        orangeRGBA, // Couleur orange
-                      ],
-                    },
-                  });
-
-                  map.current.addLayer({
-                    'id': 'cluster-border',
-                    'type': 'circle',
-                    'source': 'points',
-                    'filter': ['has', 'point_count'],
-                    'paint': {
-                      'circle-radius': 24,
-                      'circle-color': [
-                        'step',
-                        ['get', 'point_count'],
-                        greenBorderRGBA, // Couleur de bordure verte
-                        19,
-                        yellowBorderRGBA, // Couleur de bordure jaune
-                        100,
-                        orangeBorderRGBA, // Couleur de bordure orange
-                      ],
-                    },
-                  });
-              
-                map.current.addLayer({
-                  'id': 'cluster-markers',
-                  'type': 'circle',
-                  'source': 'points',
-                  'filter': ['!', ['has', 'point_count']],
-                  'paint': {
-                    'circle-radius': 6,
-                    'circle-color': '#FAC710',
-                    'circle-stroke-color': "#F9F9F9",
-                    'circle-stroke-width': 2,
-                  },
-                });
-
-                map.current.addLayer({
-                    'id': 'cluster-text',
-                    'type': 'symbol',
-                    'source': 'points',
-                    'filter': ['has', 'point_count'],
-                    'layout': {
-                      'text-field': '{point_count}',
-                      'text-font': ['Open Sans Regular'],
-                      'text-size': 13,
-                    },
-                    'paint': {
-                      'text-color': '#000000',
-                    },
-                  });
-                
-              });              
+            });
         }
     };
 
