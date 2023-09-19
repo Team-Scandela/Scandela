315e0578f3409f66b18704cba47a73b454d3b45c
Ismaël Brossaud <ismael.brossaud@epitech.eu>
2023-09-19 15:30:45 +0200

    add sentry package and sentry initialisation

--- scandelapp/src/App.tsx
+++ scandelapp/src/App.tsx
@@ -4,9 +4,24 @@ import './App.css';
 import Main from './pages/main';
 import Login from './pages/login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import * as Sentry from "@sentry/browser";
 
 /** Route page */
 const App: React.FC = () => {
+
+    Sentry.init({
+        dsn: "https://b7ba74511176b52c96d1d58dc76d7ab7@o4505907192725504.ingest.sentry.io/4505907207012352",
+        integrations: [
+            new Sentry.BrowserTracing({
+                tracePropagationTargets: ["localhost", /^https:\/\/app.scandela.fr/],
+            }),
+        ],
+        tracesSampleRate: 1.0,
+        // Session Replay
+        replaysSessionSampleRate: 0.1,
+        replaysOnErrorSampleRate: 1.0,
+    });
+    
     return (
         <BrowserRouter>
             <Routes>
