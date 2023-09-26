3bc95ce7c1055f94e414aa96bb4b5bf8c84b1489
Marvin Verain <mverain@localhost.localdomain>
2023-09-26 20:29:44 +0200

    [ADD]: adding website (only front), vitrine

---
+++ vitrine/src/carousel.js
@@ -0,0 +1,25 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#thumbnail-carousel', {
+        perPage: 3,
+        gap: '2rem',
+        snap: 'true',
+        type: "loop",
+        drag: "free",
+        focus: "center",
+        breakpoints: {
+            640: {
+              perPage: 2,
+              gap    : '.7rem',
+              height : '6rem',
+            },
+            480: {
+              perPage: 1,
+              gap    : '.7rem',
+              height : '6rem',
+            },
+          },
+        autoScroll: {
+           speed: 2,
+        },
+    } ).mount(window.splide.Extensions);
+  } );
