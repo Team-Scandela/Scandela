984be3221995996075991586f3d2c221f90454c8
Marvin Verain <mverain@localhost.localdomain>
2023-10-12 09:36:11 +0200

    [FIX]: fix failed CI with prettier

--- vitrine/src/splide.js
+++ vitrine/src/splide.js
@@ -1,18 +1,18 @@
-var splide = new Splide( '.splide', {
+var splide = new Splide('.splide', {
     perPage: 3,
-    gap    : '2rem',
+    gap: '2rem',
     breakpoints: {
-      640: {
-        perPage: 2,
-        gap    : '.7rem',
-        height : '6rem',
-      },
-      480: {
-        perPage: 1,
-        gap    : '.7rem',
-        height : '6rem',
-      },
+        640: {
+            perPage: 2,
+            gap: '.7rem',
+            height: '6rem',
+        },
+        480: {
+            perPage: 1,
+            gap: '.7rem',
+            height: '6rem',
+        },
     },
-  } );
-  
-  splide.mount();
+});
+
+splide.mount();
