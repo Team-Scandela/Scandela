e44577fb8134d7b523cc8f9dae1943d93cde2e5f
Kéon Savic <keon.savic@epitech.eu>
2023-10-22 23:38:26 +0900

    [FIX] - Entities Builder added

--- server/src/main/java/com/scandela/server/entity/LightPoint.java
+++ server/src/main/java/com/scandela/server/entity/LightPoint.java
@@ -9,15 +9,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-// import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
-// @Builder
+@Builder
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 @Table(name = "lamp")
 public class LightPoint implements Serializable {
