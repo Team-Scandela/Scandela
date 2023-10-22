379952f5290a89248818203552156f1c6137229f
Kéon Savic <keon.savic@epitech.eu>
2023-10-22 23:34:54 +0900

    [WIP] - adding services

--- server/src/main/java/com/scandela/server/entity/WhileAway.java
+++ server/src/main/java/com/scandela/server/entity/WhileAway.java
@@ -7,8 +7,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
