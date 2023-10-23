e0697a0ade6340459481b96fd2b28740552d2462
Laurent Enzo <enzo.laurent@epitech.eu>
2023-10-23 10:09:21 +0200

    [FIX]

--- server/src/main/java/com/scandela/server/controller/EmailController.java
+++ server/src/main/java/com/scandela/server/controller/EmailController.java
@@ -1,16 +1,11 @@
 package com.scandela.server.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.scandela.server.entity.User;
 import com.scandela.server.service.IEmailService;
 
 @RestController
--- server/src/main/java/com/scandela/server/controller/LoginController.java
+++ server/src/main/java/com/scandela/server/controller/LoginController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.scandela.server.entity.Login;
 import com.scandela.server.entity.User;
 import com.scandela.server.service.ILoginService;
 
