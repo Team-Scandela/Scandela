deed9d013b2d57021310d2b39e04a4dcc4964233
Kéon Savic <keon.savic@epitech.eu>
2023-12-20 20:54:20 +0900

    [FIX] - problem on Ticket Service bean

--- server/src/main/java/com/scandela/server/controller/TicketController.java
+++ server/src/main/java/com/scandela/server/controller/TicketController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.scandela.server.entity.Ticket;
-import com.scandela.server.entity.Town;
 import com.scandela.server.service.ITicketService;
 
 @RestController
--- server/src/main/java/com/scandela/server/service/implementation/TicketService.java
+++ server/src/main/java/com/scandela/server/service/implementation/TicketService.java
@@ -1,10 +1,13 @@
 package com.scandela.server.service.implementation;
 
+import org.springframework.stereotype.Service;
+
 import com.scandela.server.dao.TicketDao;
 import com.scandela.server.entity.Ticket;
 import com.scandela.server.service.AbstractService;
 import com.scandela.server.service.ITicketService;
 
+@Service
 public class TicketService extends AbstractService<Ticket> implements ITicketService {
     protected TicketService(TicketDao ticketDao) {
 		super(ticketDao);
