c5bb5dc7fe124e2710fbc03e5a35ef048b103fef
Ismaël Brossaud <ismael.brossaud@epitech.eu>
2023-12-20 12:09:47 +0100

    [ADD] front for the popup to add new bulb, with indicator next to the consommation : need to improve the front

---
+++ scandelapp/src/components/MissingLampPopup/index.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { InputWrapper } from '../SearchBar/elements';
+
+import {
+    PopupWindow,
+    IconContainer,
+    TextInput,
+    PopupTextInfoTitle,
+} from './elements';
+
+import * as images from './ficheImports';
+
+interface InfoIconPopupProps {
+    isDark: boolean;
+}
+
+const InfoIconPopup: React.FC<InfoIconPopupProps> = ({ isDark }) => {
+    const [isPopupVisible, setPopupVisible] = useState(false);
+    const [isEnteringID, setEnteringID] = useState(true);
+    const [bulbID, setBulbID] = React.useState<string>('');
+    const [bulbValue, setBulbValue] = React.useState<string>('');
+
+    const handleInfoIconClick = () => {
+        setPopupVisible(!isPopupVisible);
+        setEnteringID(true);
+    };
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (isEnteringID) {
+            setBulbID(e.target.value);
+        } else {
+            setBulbValue(e.target.value);
+        }
+    };
+
+    const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            if (isEnteringID) {
+                setEnteringID(false);
+            } else {
+                setPopupVisible(false);
+            }
+        }
+    };
+
+    return (
+        <>
+            <IconContainer>
+                <img
+                    src={images.switch_off}
+                    alt="Flash"
+                    draggable="false"
+                    style={{
+                        position: 'absolute',
+                        top: '350px',
+                        left: '350px',
+                        width: '20px',
+                        userSelect: 'none',
+                    }}
+                    onClick={handleInfoIconClick}
+                />
+            </IconContainer>
+            {isPopupVisible && (
+                <PopupWindow>
+                    <PopupTextInfoTitle>
+                        {isEnteringID ? 'Bulb ID' : 'Bulb value en Kwh'}
+                    </PopupTextInfoTitle>
+                    <TextInput isdark={isDark}>
+                        <InputWrapper
+                            placeholder={
+                                isEnteringID ? 'Bulb ID' : 'Bulb value en Kwh'
+                            }
+                            value={isEnteringID ? bulbID : bulbValue}
+                            onChange={handleInputChange}
+                            onKeyDown={handleInputKeyDown}
+                        />
+                    </TextInput>
+                </PopupWindow>
+            )}
+        </>
+    );
+};
+
+export default InfoIconPopup;
