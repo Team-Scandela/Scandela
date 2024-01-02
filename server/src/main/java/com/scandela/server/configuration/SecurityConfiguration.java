e8e176cfb3eae6991ec6544aa7e8cfd53115f427
Laurent Enzo <enzo.laurent@epitech.eu>
2024-01-02 15:05:55 +0100

    [EDIT] security open to all urls

--- server/src/main/java/com/scandela/server/configuration/SecurityConfiguration.java
+++ server/src/main/java/com/scandela/server/configuration/SecurityConfiguration.java
@@ -2,16 +2,18 @@ package com.scandela.server.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
 	@Bean
-	public SecurityFilterChain getSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.authorizeHttpRequests((authentication) -> authentication.anyRequest().authenticated())
-					.httpBasic();
-		
-		return httpSecurity.build();
-	}
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable().authorizeHttpRequests((authorize) -> {
+            authorize.anyRequest().authenticated();
+        }).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 }
