12172b16d9b27ce7dd22853ff69979b56d85ba31
TitouanDsch <titouandeschanels@users.noreply.github.com>
2024-01-22 21:55:07 +0100

    Update fetch for the java

--- scandelapp/src/components/SettingsButton/Download/index.tsx
+++ scandelapp/src/components/SettingsButton/Download/index.tsx
@@ -9,18 +9,71 @@ interface DownloadProps {
     isDark: boolean;
 }
 
+interface Lamp {
+    name: string;
+    address: string;
+    lat : number;
+    long : number;
+    lighton : string;
+    lightoff : string;
+    height : number;
+}
+
 const Download: React.FC<DownloadProps> = ({ isDark }) => {
     const fileInputRef = React.useRef<HTMLInputElement | null>(null);
 
-    function launchScript(argument: string) {
-        fetch(`http://db.scandela.fr/script`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ argument }),
-        }).then((response) => response.text());
-    }
+    const addToDB = async (data: Lamp[]) => {
+        for (const lamp of data) {
+            try {
+                const response = await fetch(
+                    'http://localhost:8080/lamps/create',
+                    {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            name: lamp.name,
+                            address: lamp.address,
+                            lat: lamp.lat,
+                            long: lamp.long,
+                            lighton: lamp.lighton,
+                            lightoff: lamp.lightoff,
+                            height: lamp.height,
+                        }),
+                    }
+                );
+                if (!response.ok) {
+                    console.error(`Failed to add ${lamp.name} to the database. Status: ${response.status}`);
+                }
+            } catch (error) {
+                console.error(`Error adding ${lamp.name} to the database: ${error.message}`);
+            }
+        }
+    };
+
+    const parseData = (data: any) => {
+        const lampList: Lamp[] = [];
+        if (Array.isArray(data)) {
+            data.forEach((element: any) => {
+                const lamp: Lamp = {
+                    name: element.fields.numero,
+                    address: element.fields.nom_voie,
+                    lat: element.geometry.coordinates[1],
+                    long: element.geometry.coordinates[0],
+                    lighton: element.fields.type_lampe,
+                    lightoff: element.fields.type_foyer,
+                    height: element.fields.hauteur_support,
+                };
+                lampList.push(lamp);
+            });
+        } else {
+            console.error(
+                'The file is not an array, please check the file format'
+            );
+        }
+        return lampList;
+    };
 
     const downloadData = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files && e.target.files.length > 0) {
@@ -29,7 +82,8 @@ const Download: React.FC<DownloadProps> = ({ isDark }) => {
             reader.onload = (evt) => {
                 if (evt.target) {
                     const fileContent = evt.target.result;
-                    launchScript(fileContent as string);
+                    const parsedData = parseData(JSON.parse(fileContent as string));
+                    addToDB(parsedData);
                 }
             };
         }
