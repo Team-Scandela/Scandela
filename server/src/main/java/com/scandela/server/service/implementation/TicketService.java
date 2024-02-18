1db759f39d6ac91033e87f27caa4125861af209a
Kéon Savic <keon.savic@epitech.eu>
2024-02-18 14:25:39 +0900

    [UPDATE] - update route

--- server/src/main/java/com/scandela/server/service/implementation/TicketService.java
+++ server/src/main/java/com/scandela/server/service/implementation/TicketService.java
@@ -1,5 +1,7 @@
 package com.scandela.server.service.implementation;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import com.scandela.server.dao.TicketDao;
@@ -9,7 +11,22 @@ import com.scandela.server.service.ITicketService;
 
 @Service
 public class TicketService extends AbstractService<Ticket> implements ITicketService {
-    protected TicketService(TicketDao ticketDao) {
+
+	private final String[] IGNORED_PROPERTIES = { "id", "author", "date"};
+
+	protected TicketService(TicketDao ticketDao) {
 		super(ticketDao);
+	}
+
+
+	@Override
+	public Ticket update(UUID id, Ticket update, String... editables) throws Exception {
+		try {
+			Ticket ticket = super.update(id, update, IGNORED_PROPERTIES);
+
+			return ticket;
+		} catch (Exception e) {
+			throw e;
+		}
 	}
 }
