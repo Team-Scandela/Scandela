08e67ad5196d5ca5525518a41597da8bb7d75838
Kéon Savic <keon.savic@epitech.eu>
2024-03-21 14:01:55 +0900

    [WIP]

--- server/src/main/java/com/scandela/server/controller/LoggerController.java
+++ server/src/main/java/com/scandela/server/controller/LoggerController.java
@@ -2,7 +2,6 @@ package com.scandela.server.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +11,4 @@ public class LoggerController {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggerController.class);
 
-    @GetMapping("/sample")
-    public String sampleEndpoint() {
-        logger.info("Sample endpoint accessed");
-        // ... Your controller logic here
-        return "Sample response";
-    }
 }
