90d3dbd429677f188b24da5d85842bd8c02bc949
TitouanDsch <titouandeschanels@users.noreply.github.com>
2024-04-17 18:00:05 +0200

    Update Menubar component styling and add hover logo

--- newVitrine/src/components/menubar/index.js
+++ newVitrine/src/components/menubar/index.js
@@ -1,11 +1,20 @@
+import {useState} from 'react';
 import {MenubarContainer, MenubarLogo, MenubarMenu, MenubarMenuItem} from './elements';
 import logo from "../../assets/logo-128x128-white.png";
+import hoverlogo from "../../assets/logo-128x128-yellow.png";
 
 const Menubar = () => {
+
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleHover = () => {
+        setIsHovered(!isHovered);
+    };
+
     return (
         <MenubarContainer>
-            <MenubarLogo >
-                <img src={logo} alt="Logo" />
+            <MenubarLogo onMouseEnter={handleHover} onMouseLeave={handleHover}>
+                <img src={isHovered ? hoverlogo : logo} alt="Logo" />
             </MenubarLogo>
             <MenubarMenu>
                 <MenubarMenuItem>A propos</MenubarMenuItem>
