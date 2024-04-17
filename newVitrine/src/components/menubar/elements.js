c22ce382a8d703d0ac409614fe56415db52f3d0b
TitouanDsch <titouandeschanels@users.noreply.github.com>
2024-04-17 13:09:54 +0200

    Update Menubar component styling and add logo

--- newVitrine/src/components/menubar/elements.js
+++ newVitrine/src/components/menubar/elements.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Yellow, Black, White } from '../../colors';
 
 export const MenubarContainer = styled.div`
     display: flex;
@@ -6,8 +7,8 @@ export const MenubarContainer = styled.div`
     align-items: center;
     height: 100px;
     padding: 0 10px;
-    background-color: #2A2B2A;
-    color: #F9F9F9;
+    background-color: ${Black};
+    color: ${White};
     font-size: 14px;
     font-weight: 600;
     position: fixed;
@@ -24,6 +25,13 @@ export const MenubarLogo = styled.div`
     height: 50px;
     margin-right: 10px;
     cursor: pointer;
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+    z-index: 101;
+    user-select: none;
 `;
 
 export const MenubarMenu = styled.div`
@@ -36,5 +44,9 @@ export const MenubarMenuItem = styled.div`
     align-items: center;
     margin: 0 10px;
     cursor: pointer;
+    user-select: none;
+    &:hover {
+        color: ${Yellow};
+    }
 `;
 
--- newVitrine/src/components/menubar/index.js
+++ newVitrine/src/components/menubar/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import {MenubarContainer} from './elements';
+import {MenubarContainer, MenubarLogo, MenubarMenu, MenubarMenuItem} from './elements';
+import logo from "../../assets/logo-128x128-white.png";
 
 const Menubar = () => {
     return (
         <MenubarContainer>
-            Taskbar
+            <MenubarLogo >
+                <img src={logo} alt="Logo" />
+            </MenubarLogo>
+            <MenubarMenu>
+                <MenubarMenuItem>Home</MenubarMenuItem>
+                <MenubarMenuItem>Products</MenubarMenuItem>
+                <MenubarMenuItem>Services</MenubarMenuItem>
+                <MenubarMenuItem>Contact</MenubarMenuItem>
+            </MenubarMenu>
         </MenubarContainer>
     );
 };
