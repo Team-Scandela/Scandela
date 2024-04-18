e04147266867da6368016c1cc56499c8945d9959
TitouanDsch <titouandeschanels@users.noreply.github.com>
2024-04-18 22:18:17 +0200

    Add Timeline component styling

---
+++ newVitrine/src/components/timeline/elements.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { Grey } from '../../colors';
+
+export const TimelineContainer = styled.div`
+    background-color: ${Grey};
+`;
--- newVitrine/src/components/timeline/index.js
+++ newVitrine/src/components/timeline/index.js
@@ -1,57 +1,98 @@
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import { Yellow, Black, White } from '../../colors';
+import { Yellow, White } from '../../colors';
+import { TimelineContainer } from './elements';
 
 const Timeline = () => {
     return (
-        <VerticalTimeline>
-            <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: Yellow, color: White }}
-                contentArrowStyle={{ borderRight: '7px solid' +  Yellow }}
-                date="2011 - present"
-                iconStyle={{ background: Yellow, color: White }}
-            >
-                <h3 className="vertical-timeline-element-title">Creative Director</h3>
-                <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-                <p>
-                    Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-                </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                date="2010 - 2011"
-                iconStyle={{ background: Yellow, color: White }}
-            >
-                <h3 className="vertical-timeline-element-title">Art Director</h3>
-                <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                <p>
-                    Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-                </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                date="2008 - 2010"
-                iconStyle={{ background: Yellow, color: White }}
-            >
-                <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-                <p>
-                    User Experience, Visual Design
-                </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                date="2006 - 2008"
-                iconStyle={{ background: Yellow, color: White }}
-            >
-                <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                <p>
-                    User Experience, Visual Design
-                </p>
-            </VerticalTimelineElement>
-        </VerticalTimeline>
+        <TimelineContainer>
+            <VerticalTimeline>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    // contentStyle={{ background: Yellow, color: White }}
+                    // contentArrowStyle={{ borderRight: '7px solid' +  Yellow }}
+                    date="Octobre - Décembre 2024"
+                    iconStyle={{ background: Yellow, color: White }}
+                >
+                    <h3 className="vertical-timeline-element-title">Launch & Metrics</h3>
+                    <p>
+                        Lancement du projet, msie en place des outils nécessaires au suivi d'un projet en production
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="Juin - Septembre 2024"
+                    iconStyle={{ background: Yellow, color: White }}
+                >
+                    <h3 className="vertical-timeline-element-title">Consolidation</h3>
+                    <p>
+                        Ecouter les retours et faire les modifications nécessaires pour améliorer l'expérience utilisateur
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    contentStyle={{ background: Yellow, color: White }}
+                    contentArrowStyle={{ borderRight: '7px solid' +  Yellow }}
+                    date="Février - Mai 2024"
+                    iconStyle={{ background: Yellow, color: White }}
+                >
+                    <h3 className="vertical-timeline-element-title">Beta</h3>
+                    <p>
+                        Mise en place de la version beta du projet pour recueillir les premiers retours des utilisateurs
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="Octobre 2023 - Janvier 2024"
+                    iconStyle={{ background: Yellow, color: White }}
+                >
+                    <h3 className="vertical-timeline-element-title">Fast Forward</h3>
+                    <p>
+                        Avancée rapide sur le projet pour atteindre les objectifs fixés dans le developpement du projet
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="Juin - Octobre 2023"
+                    iconStyle={{ background: Yellow, color: White }}
+                >
+                    <h3 className="vertical-timeline-element-title">Management & Process</h3>
+                    <p>
+                        Structure et pérenniser l'organisation au sein du groupe et de la gestion du projet
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="Avril - Juin 2023"
+                    iconStyle={{ background: Yellow, color: White }}
+                >
+                    <h3 className="vertical-timeline-element-title">Test & Learn</h3>
+                    <p>
+                        Période de test, méthodes d'organisations internes, et moyens de communications
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="Décembre 2022"
+                    iconStyle={{ background: Yellow, color: White }}
+                >
+                    <h3 className="vertical-timeline-element-title">Pisicne Forward</h3>
+                    <p>
+                        Elaboration d'un plan de recherche sur les besoins des utilisateurs et les solutions à apporter, les concuurent sur le marché, développement du buisness plan et création d'un premier MVP
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="Septembre 2022"
+                    iconStyle={{ background: Yellow, color: White }}
+                >
+                    <h3 className="vertical-timeline-element-title">Pisicne Moonshot</h3>
+                    <p>
+                        Création et imagination du projet, recherche de solutions innovantes et de valeur ajoutée
+                    </p>
+                </VerticalTimelineElement>
+            </VerticalTimeline>
+        </TimelineContainer>
 
     );
 }
