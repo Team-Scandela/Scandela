b1ae8711f9c13077189a2ebb114e4273a8f09252
TitouanDsch <titouandeschanels@users.noreply.github.com>
2024-04-19 11:47:50 +0200

    Update Album component with team members' information and images

--- newVitrine/src/components/album/index.js
+++ newVitrine/src/components/album/index.js
@@ -1,4 +1,9 @@
 import { AlbumContainer, AlbumLine, AlbumPic, AlbumTitle, AlbumBox, AlbumText } from "./elements";
+import ib from "../../assets/portrait/ib.jpeg";
+import na from "../../assets/portrait/na.jpg";
+import td from "../../assets/portrait/td.jpeg";
+import vh from "../../assets/portrait/vh.jpeg";
+import ks from "../../assets/portrait/ks.jpeg";
 
 
 const Album = () => {
@@ -7,24 +12,24 @@ const Album = () => {
             <AlbumLine>
                 <AlbumTitle>Notre équipe</AlbumTitle>
                 <AlbumBox>
-                    <AlbumPic src="https://via.placeholder.com/200" alt=""/>
+                    <AlbumPic src={na} alt=""/>
                     <AlbumText>Nassim Alaimi</AlbumText>
                     <AlbumText>Développeur Fullstack</AlbumText>
                 </AlbumBox>
                 <AlbumBox>
-                    <AlbumPic src="https://via.placeholder.com/200" alt=""/>
+                    <AlbumPic src={ib} alt=""/>
                     <AlbumText>Ismael Brossaud</AlbumText>
                     <AlbumText>Développeur Backend</AlbumText>
                 </AlbumBox>
                 <AlbumBox>
-                    <AlbumPic src="https://via.placeholder.com/200" alt=""/>
+                    <AlbumPic src={td} alt=""/>
                     <AlbumText>Titouan Deschanels</AlbumText>
                     <AlbumText>Développeur Fullstack</AlbumText>
                 </AlbumBox>
             </AlbumLine>
             <AlbumLine>
                 <AlbumBox>
-                    <AlbumPic src="https://via.placeholder.com/200" alt=""/>
+                    <AlbumPic src={vh} alt=""/>
                     <AlbumText>Victor Harri-Chal</AlbumText>
                     <AlbumText>Développeur Frontend</AlbumText>
                 </AlbumBox>
@@ -34,7 +39,7 @@ const Album = () => {
                     <AlbumText>Développeur Backend</AlbumText>
                 </AlbumBox>
                 <AlbumBox>
-                    <AlbumPic src="https://via.placeholder.com/200" alt=""/>
+                    <AlbumPic src={ks} alt=""/>
                     <AlbumText>Kéon Savic</AlbumText>
                     <AlbumText>Développeur Backend</AlbumText>
                 </AlbumBox>
