d5f2adc9dab94a24448ff718023a1168110dccdf
TitouanDsch <titouandeschanels@users.noreply.github.com>
2024-04-23 11:04:04 +0200

    Refactor executable package.json and Main.js files

--- executable/Main.js
+++ executable/Main.js
@@ -1,10 +1,20 @@
 const {app, BrowserWindow} = require('electron')
 function createWindow () {
   // Create the browser window.
-    let win = new BrowserWindow({width: 800, height: 600})
-    win.webContents.openDevTools();
-    win.loadURL('http://localhost:3000');
+    let win = new BrowserWindow({width: 1920, height: 1080})
+    // win.webContents.openDevTools();
+    win.loadURL('https://app.scandela.fr');
 }
 // and load the index.html of the app.     win.loadFile('index.html')   }
 app.on('ready', createWindow);
 
+app.on("closed", function (){
+
+  app = null
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
