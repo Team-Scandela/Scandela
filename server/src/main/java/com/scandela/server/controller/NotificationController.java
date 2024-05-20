24915bda9c15502cc80d08aacf662f3a0d867edc
Laurent Enzo <enzo.laurent@epitech.eu>
2024-05-20 16:06:15 +0200

    [ADD] notifications

---
+++ server/src/main/java/com/scandela/server/controller/NotificationController.java
@@ -0,0 +1,89 @@
+package com.scandela.server.controller;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.scandela.server.entity.Notification;
+import com.scandela.server.exception.NotificationException;
+import com.scandela.server.service.INotificationService;
+
+@RestController
+@RequestMapping(value = "/notifications")
+@CrossOrigin(origins = "*")
+public class NotificationController extends AbstractController<Notification> {
+
+	// Constructors \\
+	protected NotificationController(INotificationService notificationService) {
+		super(notificationService);
+	}
+
+	// Methods \\
+	// Public \\
+	/**
+	 * Get all notifications
+	 * 
+	 * @return allNotifications
+	 */
+	@GetMapping
+	public List<Notification> getNotifications(@RequestParam UUID idUser) {
+		return ((INotificationService) service).getAll(idUser);
+	}
+
+	/**
+	 * Get notification by id
+	 * 
+	 * @param id
+	 * @return notification
+	 */
+	@GetMapping("/{id}")
+	public Notification getNotification(@PathVariable UUID id) {
+		return super.get(id);
+	}
+
+	/**
+	 * Create new notification
+	 * 
+	 * @param newNotification
+	 * @return newNotification
+	 * @throws NotificationException 
+	 */
+	@PostMapping("/create")
+	public Notification createNotification(@RequestBody Notification newNotification) throws Exception {
+		return super.create(newNotification);
+	}
+
+	/**
+	 * Update notification by id
+	 * 
+	 * @param id
+	 * @param update
+	 * @return
+	 * @throws Exception
+	 */
+    @PutMapping("/{id}")
+    public Notification updateNotification(@PathVariable UUID id, @RequestBody Notification update) throws Exception {
+        return super.update(id, update);
+    }
+
+	/**
+	 * Delete notification
+	 * 
+	 * @param id
+	 */
+	@DeleteMapping("/delete/{id}")
+	public void deleteNotification(@PathVariable UUID id) {
+		super.delete(id);
+	}
+
+}
