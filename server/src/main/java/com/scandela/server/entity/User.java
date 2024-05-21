50b5998555b50f8b14ea7df811d3c4a1b513ecd1
Marvin Verain <mverain@localhost.localdomain>
2024-05-21 10:18:10 +0200

    [UPDATE]: updating fixed back

--- server/src/main/java/com/scandela/server/entity/User.java
+++ server/src/main/java/com/scandela/server/entity/User.java
@@ -9,14 +9,17 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
--- server/src/main/java/com/scandela/server/service/INotificationService.java
+++ server/src/main/java/com/scandela/server/service/INotificationService.java
@@ -1,10 +1,10 @@
-Npackage com.scandela.server.service;
+package com.scandela.server.service;
 
 import java.util.List;
 import java.util.UUID;
-
 import com.scandela.server.entity.Notification;
 
 public interface INotificationService extends IService<Notification> {
-	public List<Notification> getAll(UUID idUser);
+    public List<Notification> getAll(UUID idUser);
+    public List<Notification> getLatest(UUID idUser, int limit);
 }
