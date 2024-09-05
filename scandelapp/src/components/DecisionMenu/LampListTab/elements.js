f6ce57c21ea1390def762a4fe621e7377f0aaefa
titouandeschanels <titouan.deschanels@epitech.eu>
2024-09-05 15:39:18 +0200

    chore: Add sorting and filtering functionality to LampListTab

--- scandelapp/src/components/DecisionMenu/LampListTab/elements.js
+++ scandelapp/src/components/DecisionMenu/LampListTab/elements.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import { Black, DarkGrey, Yellow, White, Grey, DarkYellow, LightDarkGrey } from '../../../colors';
+import { BiSortDown } from "react-icons/bi";
+import { FaFilter } from "react-icons/fa";
 
 export const LampListContainer = styled.div`
     display: flex;
@@ -9,9 +11,114 @@ export const LampListContainer = styled.div`
     transform: translate(-50%, 0%);
     width: 94%;
     height: 80%;
-    overflow: visible;
+    overflow-y: auto;
     z-index: 1;
     user-select: none;
     background-color: ${Black};
     border-radius: 5px;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    gap: 10px;
+    &::-webkit-scrollbar {
+        width: 10px;
+    }
+    &::-webkit-scrollbar-thumb {
+        background-color: ${White + '99'};
+        border-radius: 10px;
+    }
+`;
+
+export const LampListCard = styled.div`
+    display: flex;
+    position: relative;
+    width: calc(100% - 20px);
+    background-color: ${DarkGrey};
+    height: 100px;
+    padding: 10px;
+    gap: 10px;
+    transition: background-color 0.3s;
+    min-height: 100px;
+    border-radius: 5px;
+    &:hover {
+        background-color: ${LightDarkGrey};
+    }
+`;
+
+export const LampListCardTitle = styled.div`
+    position: absolute;
+    user-select: none;
+    top: 5px;
+    font-weight: bold;
+    font-size: 25px;
+    top : 10px;
+    left : 10px;
+    color : ${Yellow};
+`;
+
+export const LampListCardAdress = styled.div`
+    position: absolute;
+    user-select: none;
+    top: 42.5px;
+    left: 10px;
+    color : ${White};
+    font-style: italic;
+    font-size: 20px;
+`;
+
+export const LampListCardBulb = styled.div`
+    position: absolute;
+    user-select: none;
+    top: 70px;
+    left: 10px;
+    color : ${White};
+    font-size: 20px;
+`;
+
+
+export const LampListCardInput = styled.input`
+    position: absolute;
+    width: 70%;
+    height: 40px;
+    top: 10px;
+    left: 3%;
+    border: none;
+    border-radius: 5px;
+    background-color: ${Black};
+    color : ${White};
+    user-select: none;
+    font-size: 20px;
+    padding-left: 10px;
+    &:focus {
+        outline: none;
+    }
+`;
+
+export const LampListOrderButton = styled(BiSortDown)`
+    position: absolute;
+    right: 75px;
+    top: 10px;
+    font-size: 40px;
+    color: ${Yellow};
+    opacity: 1;
+    transform: translateX(-50%);
+    &:hover {
+        cursor: pointer;
+        opacity: 0.8;
+    }
+`;
+
+export const LampListFilterButton = styled(FaFilter)`
+    position: absolute;
+    right: 20px;
+    top: 15px;
+    font-size: 30px;
+    color: ${Yellow};
+    opacity: 1;
+    transform: translateX(-50%);
+    &:hover {
+        cursor: pointer;
+        opacity: 0.8;
+    }
 `;
--- scandelapp/src/components/DecisionMenu/LampListTab/index.tsx
+++ scandelapp/src/components/DecisionMenu/LampListTab/index.tsx
@@ -1,16 +1,72 @@
 import {
     LampListContainer,
+    LampListCard,
+    LampListCardTitle,
+    LampListCardAdress,
+    LampListCardBulb,
+    LampListCardInput,
+    LampListOrderButton,
+    LampListFilterButton,
 } from './elements';
 
 interface LampListTabProps {
     isDark: boolean;
 }
 
+let data = [
+    {
+        title : "Lamp 1",
+        adress : "Rue de la paix",
+        bulb : "LED",
+    },
+    {
+        title : "Lamp 2",
+        adress : "Rue de la dljfgdf",
+        bulb : "LED",
+    },
+    {
+        title : "Lamp 3",
+        adress : "Rue de la retfghbn",
+        bulb : "LPHP",
+    },
+    {
+        title : "Lamp 4",
+        adress : "Rue de la rsdjhskdjhsd",
+        bulb : "OUI",
+    },
+    {
+        title : "Lamp 4",
+        adress : "Rue de la rsdjhskdjhsd",
+        bulb : "OUI",
+    },
+    {
+        title : "Lamp 4",
+        adress : "Rue de la rsdjhskdjhsd",
+        bulb : "OUI",
+    },
+    {
+        title : "Lamp 4",
+        adress : "Rue de la rsdjhskdjhsd",
+        bulb : "OUI",
+    }
+]
+
 const LampListTab: React.FC<LampListTabProps> = ({ isDark }) => {
 
     return (
         <div>
-            <LampListContainer />
+            <LampListCardInput placeholder="Search" />
+            <LampListOrderButton />
+            <LampListFilterButton />
+            <LampListContainer >
+                {data.map((lamp, index) => (
+                    <LampListCard key={index} isDark={isDark}>
+                        <LampListCardTitle>{lamp.title}</LampListCardTitle>
+                        <LampListCardAdress>{lamp.adress}</LampListCardAdress>
+                        <LampListCardBulb>{lamp.bulb}</LampListCardBulb>
+                    </LampListCard>
+                ))}
+            </LampListContainer>
         </div>
     );
 }
