c36832d87f5a192e8e6aaa0ffa0d654ea702eafb
Kéon Savic <keon.savic@epitech.eu>
2024-09-10 19:24:43 +0900

    [UP] - finished aspect security check

--- server/src/main/java/com/scandela/server/util/AdminVilleAccess.java
+++ server/src/main/java/com/scandela/server/util/AdminVilleAccess.java
@@ -1,3 +1,5 @@
+package com.scandela.server.util;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
--- server/src/main/java/com/scandela/server/util/AdminVilleAccessAspect.java
+++ server/src/main/java/com/scandela/server/util/AdminVilleAccessAspect.java
@@ -1,5 +1,27 @@
 package com.scandela.server.util;
 
+import java.nio.file.AccessDeniedException;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import com.scandela.server.entity.User;
+
+@Aspect
+@Component
 public class AdminVilleAccessAspect {
-    
+
+    @Around("@annotation(AdminVilleAccess)")
+    public Object checkAdminVilleAccess(ProceedingJoinPoint joinPoint) throws Throwable {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (currentUser.isAdminville()) {
+            return joinPoint.proceed();
+        } else {
+            throw new AccessDeniedException("Accès refusé. Vous devez être Admin_Ville.");
+        }
+    }
 }
