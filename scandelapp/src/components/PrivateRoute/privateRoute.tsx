254c3c0f0a049c075939cba07761472c3364a3a3
Marvin Verain <marvin.verain@epitech.eu>
2024-09-30 02:07:35 +0200

    [FORMAT]: fixed format with prettier

--- scandelapp/src/components/PrivateRoute/privateRoute.tsx
+++ scandelapp/src/components/PrivateRoute/privateRoute.tsx
@@ -1,13 +1,13 @@
 import { Navigate } from 'react-router-dom';
 
 interface PrivateRouteProps {
-  children: JSX.Element;
+    children: JSX.Element;
 }
 
 const PrivateRoute = ({ children }: PrivateRouteProps) => {
-  const isAuthenticated = localStorage.getItem('token'); // on prend le token dans le local storage pour check si l'utilisateur est login mais à terme il faudra un peu plus sécurisé
+    const isAuthenticated = localStorage.getItem('token'); // on prend le token dans le local storage pour check si l'utilisateur est login mais à terme il faudra un peu plus sécurisé
 
-  return isAuthenticated ? children : <Navigate to="/" />; // si l'utilisateur est login on retourne le composant sinon on le redirige vers la page de login
+    return isAuthenticated ? children : <Navigate to="/" />; // si l'utilisateur est login on retourne le composant sinon on le redirige vers la page de login
 };
 
 export default PrivateRoute;
