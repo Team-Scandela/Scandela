f5dfc51ce5aa91ece920606de76caa6cbd3330aa
Marvin Verain <marvin.verain@epitech.eu>
2024-11-03 07:12:18 +0100

    [UPD]: improving anti bypass by removing localstorage at login page

--- scandelapp/src/pages/login.tsx
+++ scandelapp/src/pages/login.tsx
@@ -1,4 +1,5 @@
 import LoginModule from '../components/LoginModule';
+import React, { useEffect } from 'react';
 
 interface LoginProps {
     setOptimisationTemplateData: (data: any) => void;
@@ -10,6 +11,12 @@ const Login: React.FC<LoginProps> = ({
     setOptimisationTemplateData,
     addItemToOptimisationTemplate,
 }) => {
+
+    // on efface le localstorage sur le login pour empecher le bypass par sauvegarde du token
+    useEffect(() => {
+        localStorage.clear();
+    }, []);
+
     return (
         <div
             id={'loginPage'}
