dcbe18a9a8cfa19c5989785cca9abf1c62834c20
Laurent Enzo <enzo.laurent@epitech.eu>
2024-11-06 11:19:19 +0100

    [ADD] Get weather every 2 hours

--- server/src/main/java/com/scandela/server/task/DecisionTask.java
+++ server/src/main/java/com/scandela/server/task/DecisionTask.java
@@ -8,14 +8,26 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.scandela.server.service.IDecisionService;
+
 @Component
 public class DecisionTask {
 	
-	@Scheduled(fixedRate = 3600000)
-	public void testTask() {
+	private IDecisionService decisionService;
+	
+	// Constructors \\
+	protected DecisionTask(IDecisionService decisionService) {
+		this.decisionService = decisionService;
+	}
+	
+	@Scheduled(fixedRate = 7205000)
+	public void taskGetWeather() throws Exception {
+		//TODO delete toutes les decisions existantes du weather et faire en sorte que la génération s'applique sur tous les lampadaires pas seulement sur un page -> verifier que ca bloque pas le reste
+		decisionService.algoReductionConsoHoraireWeather();
+		
 		Logger log = LoggerFactory.getLogger(DecisionTask.class);
 		SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 		
-		log.info("The time is now {}", dateFormat.format(new Date()));
+		log.info("Weather get at time {}", dateFormat.format(new Date()));
 	}
 }
