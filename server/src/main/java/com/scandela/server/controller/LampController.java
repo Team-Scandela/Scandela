3cbc86e476f3c902e89e3ae94816e2e0e3d81394
Kéon Savic <keon.savic@epitech.eu>
2024-11-23 22:28:36 +0100

    [FIX] - MMDR le fix

--- server/src/main/java/com/scandela/server/controller/LampController.java
+++ server/src/main/java/com/scandela/server/controller/LampController.java
@@ -93,7 +93,7 @@ public class LampController extends AbstractController<Lamp> {
         return new ArrayList<>(allOtherLamps);
     }
 
-	@GetMapping("/{townId}")
+	@GetMapping("/fromtown/{townId}")
 	public List<LampDto> getLampsFromTown(@PathVariable UUID townId) {
 		System.out.println("Given townId == -> " + townId);
 
